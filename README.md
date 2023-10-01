b33f055d2a125f2dbbfad4cac2c35bf33aadb70f
Malath-Yasin <yasinmalath@gmail.com>
2023-10-01 19:29:20 +0300

    add while loop and for loop in app,js file

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## caffeZ project it's a website for a caffe, allow the user to enter their order, display it and check the status .
 
+## I use loop in app.js file , i used while and for loop .
+
 ## live url
 
 -(live url for caffeZ )[https://malath-yasin.github.io/CaffeZ/]
--- app.js
+++ app.js
@@ -3,17 +3,18 @@
   
  if ( Name != null){
    var gender=prompt("Please Enter Your Gender","Gender");
-  
-   if(gender =="female" || gender =="Female" ){
+//EX2 LOOPS 
+   while (gender.toLocaleLowerCase() !== "female" && gender.toLocaleLowerCase() !== "male"  ){
+    gender=prompt("ERROR : please enter your gender again ","Gender");
+  }
+   if(gender.toLocaleLowerCase() =="female"){
     alert("Welcome To CaffeZ Ms "+Name);
    }
-   else if (gender =="male" ||gender =="Male"){
+   else if (gender.toLocaleLowerCase() =="male" ){
     alert("Welcome To CaffeZ Mr "+Name);
-   } else{
-    alert("Welcome To CaffeZ "+Name);
-   }
- }
+   } 
 
+ }
    var drink =prompt("You want to drink (hot drink ) or (cold drink)","hot/cold");
     if(drink !=null){
    var drinkName =prompt("Write the name of your drink please ","Drink name here");
@@ -24,3 +25,8 @@
           }
     }
 console.log("The Order for " +Name+" is "+drinkName );
+//EX2 LOOPS 
+var arr=[Name,gender,drink,drinkName];
+for(var i=0;i<4;i++){
+  console.log(arr[i]);
+}
